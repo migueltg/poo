c5cdfd91c9a0d87b9aa767558625e9d8fc780792
miw-upm <miw@eui.upm.es>
2014-08-31 11:10:30 +0200

    point v2

---
+++ src/main/java/utils/v1/Point.java
@@ -0,0 +1,25 @@
+package utils.v1;
+
+public class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+    }
+
+    public Point(int xy) {
+    }
+
+    public Point() {
+    }
+
+    public double module() {
+        return 0;
+    }
+
+    public double phase() {
+        return 0;
+    }
+
+    public void translate(Point origin) {
+    }
+}
