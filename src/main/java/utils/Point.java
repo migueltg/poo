46a893bb6d8fa0a829dd547f839d6111dd01e4d9
miw-upm <miw@eui.upm.es>
2014-08-31 13:01:22 +0200

    point implementación

--- src/main/java/utils/Point.java
+++ src/main/java/utils/Point.java
@@ -1,33 +1,39 @@
-package utils.v1;
+package utils;
 
 public class Point {
     private int x, y;
 
     public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     public Point(int xy) {
+        this(xy, xy);
     }
 
     public Point() {
+        this(0, 0);
     }
 
     public int getX() {
-        return 0;
+        return this.x;
     }
 
     public int getY() {
-        return 0;
+        return this.x;
     }
 
     public double module() {
-        return 0;
+        return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 
     public double phase() {
-        return 0;
+        return Math.atan((double) this.y / this.x);
     }
 
     public void translate(Point origin) {
+        this.x -= origin.getX();
+        this.y -= origin.getY();
     }
 }
