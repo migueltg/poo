b259d499ab3092435779fd27aaff6b676b8fce12
miw-upm <miw@eui.upm.es>
2014-09-01 10:34:20 +0200

    TimePoint

---
+++ src/main/java/utils/TimePoint.java
@@ -0,0 +1,32 @@
+package utils;
+
+public class TimePoint extends Point {
+    private int t;
+
+    public TimePoint() {
+        this(0, 0, 0);
+    }
+
+    public TimePoint(int x, int y, int t) {
+        super(x, y); // Si no se especifica, es: super();
+        this.setT(t);
+    }
+
+    public int getT() {
+        return this.t;
+    }
+
+    private void setT(int t) {
+        assert t >= 0 : "tiempo negativo es inválido";
+        this.t = t;
+    }
+
+    public void addTime(int t) {
+        this.setT(this.t + t);
+    }
+
+    public double speed() {
+        return this.module() / this.getT();
+    }
+
+}
