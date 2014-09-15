c7396175ae52e88761c72dbdd1b9d92b632f70ac
miw-upm <miw@eui.upm.es>
2014-09-15 17:00:59 +0200

    trunk

--- src/test/java/utils/v1/TrunkImpTest.java
+++ src/test/java/utils/v1/TrunkImpTest.java
@@ -1,6 +1,9 @@
 package utils.v1;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
